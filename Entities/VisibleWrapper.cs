9ad8ce806cce5bf3989a63db8cccb9967104ee68
Jose Cardoso <josenunocardoso@gmail.com>
2021-01-28 20:19:34 +0000

    VisibleWrapper child now has the same key as the parent

--- Entities/VisibleWrapper.cs
+++ Entities/VisibleWrapper.cs
@@ -2,16 +2,21 @@ using System;
 
 namespace SceneDisplayer.Entities {
     public class VisibleWrapper : Entity {
-        
+        private Entity entity;
+
         public VisibleWrapper(Entity entity, bool visible) {
-            this.AddChild("Child", entity);
+            this.entity = entity;
         }
 
 
         public bool Visible { get; set; }
 
-        public Entity Entity => this.Children["Child"];
+        public Entity Entity => this.Children[this.Key];
 
+
+        public override void Init() {
+            this.AddChild(this.Key, this.entity);
+        }
 
         public override void Draw(IntPtr renderer, int screenWidth, int screenHeight) {
             if (!this.Visible) return;
